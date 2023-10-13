2f2850f3b0f040fc8b69c2f57fa76fa68901a945
Yuliya <derilovayuliya@gmail.com>
2023-10-13 13:55:07 +0300

    3

--- src/basetask/BeeRunner.java
+++
@@ -1,8 +0,0 @@
-package basetask;
-
-public class BeeRunner {
-    public static void main(String[] args) {
-        Bee bee = new Bee("male", 50L);
-        bee.printBeeDetails();
-    }
-}
--- src/pretask/BooleanExample.java
+++
@@ -1,8 +0,0 @@
-package pretask;
-
-public class BooleanExample {
-    public static void main(String[] args) {
-        boolean a = true;
-        System.out.println(a);
-    }
-}
